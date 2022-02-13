8e0fc890ad59da4414ef5d6479273130862691eb
Zhaofei <1281041324@qq.com>
2022-02-13 14:15:51 +0800

    将数据按95：5分成训练集和验证集

--- data preprocessing.py
+++ data preprocessing.py
@@ -16,7 +16,8 @@ EPSILON = 1e-7
 noise_path = '/data01/zhaofei/data/Fusion-Net/noise'
 speech_path = '/data01/zhaofei/data/Voice-Bank/clean_trainset_28spk_wav'
 uncut_mix_path = '/data01/zhaofei/data/Fusion-Net/uncut_mix/'
-train_path = '/data01/zhaofei/data/Fusion-Net/train_data/'
+train_noisy_path = '/data01/zhaofei/data/Fusion-Net/cut_mix/'
+train_speech_path = '/data01/zhaofei/data/Fusion-Net/cut_speech/'
 snr_list = [0, 5, 10, 15]
 
 def mix2signal(sig1, sig2, snr):
@@ -45,21 +46,42 @@ mix_lst = list(Path(uncut_mix_path).rglob('*.wav'))
 #             sf.write(uncut_mix_path + mix_name, mixture, 16000)
 #
 ''''''
-# 按照16384个样本进行切割
 
-for i in tqdm.tqdm(mix_lst):
-    mixture,_ = sf.read(str(i))
+# # 对mix按照16384个样本进行切割
+# for i in tqdm.tqdm(mix_lst):
+#     mixture,_ = sf.read(str(i))
+# 
+#     label = 0
+#     t = 0
+#     while (True):
+#         if label + 16384 > mixture.shape[0]:
+#             ones = np.zeros(16384)
+#             ones[:mixture.shape[0] - label] = mixture[label:]
+#             sf.write(train_path + i.stem + '-' + str(t) + '.wav', ones, samplerate=16000)
+#             break
+#         split_mix = mixture[label: label + 16384]
+#         sf.write(train_path+i.stem+'-'+str(t)+'.wav', split_mix, samplerate=16000)
+#         label = label + 8192
+#         t = t + 1
+''''''
+      
+# 对clean按照16384个样本进行切割
+sum = 0
+for i in tqdm.tqdm(speech_lst):
+    speech,_ = sf.read(str(i))
 
     label = 0
     t = 0
     while (True):
-        if label + 16384 > mixture.shape[0]:
+        if label + 16384 > speech.shape[0]:
             ones = np.zeros(16384)
-            ones[:mixture.shape[0] - label] = mixture[label:]
-            sf.write(train_path + i.stem + '-' + str(t) + '.wav', ones, samplerate=16000)
+            ones[:speech.shape[0] - label] = speech[label:]
+            sf.write(train_speech_path + i.stem + '-' + str(t) + '.wav', ones, samplerate=16000)
             break
-        split_mix = mixture[label: label + 16384]
-        sf.write(train_path+i.stem+'-'+str(t)+'.wav', split_mix, samplerate=16000)
+        split_mix = speech[label: label + 16384]
+        sf.write(train_speech_path+i.stem+'-'+str(t)+'.wav', split_mix, samplerate=16000)
         label = label + 8192
         t = t + 1
+    sum = sum + t + 1
+print(sum)
 
---
+++ utils/split_eval_train.py
@@ -0,0 +1,34 @@
+import random
+import soundfile as sf
+from tqdm import tqdm
+import path
+import numpy as np
+import os
+from pathlib import Path
+
+if __name__ == '__main__':
+    # name_lst=list(Path("/data01/spj/ai_shell4_vad/TRAIN/seg_wav/").rglob('*.wav'))
+    speech_path = '/data01/zhaofei/data/Fusion-Net/cut_speech/'
+    npy_path = '/data01/zhaofei/data/Fusion-Net/'
+    mix_lst=list(Path("/data01/zhaofei/data/Fusion-Net/cut_mix/").rglob('*.wav'))
+    random.shuffle(mix_lst)
+
+    len_lst=len(mix_lst)
+    speech_lst = []
+
+    for i in tqdm(mix_lst):
+        speech_name = speech_path + (i.stem).split('&')[0]+ '-' + (i.stem).split('-')[1]+'.wav'
+        speech_lst.append(speech_name)
+        # a,_ = sf.read(speech_name)
+    # print("over")
+
+    train_mix_lst = mix_lst[:int(0.95*len_lst)]
+    eval_mix_lst = mix_lst[int(0.95 * len_lst):]
+    train_speech_lst = speech_lst[:int(0.95*len_lst)]
+    eval_speech_lst = speech_lst[int(0.9*len_lst):]
+
+    np.save(npy_path+"train_mix.npy",train_mix_lst)
+    np.save(npy_path+"train_speech.npy",train_speech_lst)
+    np.save(npy_path+"val_mix.npy", eval_mix_lst)
+    np.save(npy_path+"val_speech.npy", eval_speech_lst)
+    print("列表已经生成")
